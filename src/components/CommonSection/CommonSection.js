aa8252ee8207904c70dabfbd010ce1cc45831b3e
krSeonghyeon <ya41921174@gmail.com>
2025-05-25 15:24:07 +0900

    fix: 로딩창 사진 contain으로 변경 및 사진 변경

--- src/components/CommonSection/CommonSection.js
+++ src/components/CommonSection/CommonSection.js
@@ -8,10 +8,10 @@ const CommonUploadSection = ({ onUpload, imageUrl, onRequestModelModal }) => {
   const recommendedImages = [
     'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-04-01/4d97c180-d716-413d-a213-59906df1a650.jpg',
     'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-23/03e4751b-4979-4fe8-a760-a8a1f0261100.jpg',
-    'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-24/c8153900-5dcc-42a3-bfe6-2cd86ebd09d8.jpg',
-    'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-24/af6af41f-a831-4fdc-8008-e41b67c6a8e5.jpg',
     'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-24/e4071372-7513-4c3f-b6be-92002e559cbb.jpg',
     'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-24/d86a6ee0-49c0-4995-8334-816cd1f1b8d4.jpg',
+    'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-25/6e9f725b-656e-4761-9c60-b47ed5a8e7df.jpg',
+    'https://2dfittingroom.s3.ap-northeast-2.amazonaws.com/2025-05-24/af6af41f-a831-4fdc-8008-e41b67c6a8e5.jpg',
   ];
 
   const handleRecommendedSelect = (url) => {
